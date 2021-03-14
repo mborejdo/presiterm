aeb95df573aeb6ece6755b57af1967f45904ec5d
Michael Borejdo <mborejdo+github@gmail.com>
2021-03-14 11:27:55 +0100

    format with rls

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
 use anyhow::{anyhow, Context};
+use clap::{App, Arg};
+use figlet_rs::FIGfont;
+use fs::File;
+use image::GenericImageView;
+use ron::de::from_reader;
 use serde::Deserialize;
-use clap::{Arg, App};
+use std::sync::Arc;
+use std::{fs, path::Path, process::Command};
+use syntect::easy::HighlightLines;
+use syntect::highlighting::{Style, ThemeSet};
+use syntect::parsing::SyntaxSet;
+use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
+use termimad::{Area, MadSkin};
 use termwiz::caps::Capabilities;
 use termwiz::cell::AttributeChange;
 use termwiz::color::AnsiColor;
 use termwiz::color::ColorAttribute;
 use termwiz::input::{InputEvent, KeyCode, KeyEvent};
-use termwiz::surface::{Change, Position, Surface, CursorVisibility};
-use termwiz::terminal::buffered::BufferedTerminal;
-use termwiz::terminal::{new_terminal, Terminal};
-use image::GenericImageView;
-use std::sync::Arc;
 use termwiz::surface::change::ImageData;
 use termwiz::surface::TextureCoordinate;
+use termwiz::surface::{Change, CursorVisibility, Position, Surface};
+use termwiz::terminal::buffered::BufferedTerminal;
+use termwiz::terminal::{new_terminal, Terminal};
 use termwiz::Error;
-use std::{
-    fs,
-    path::Path,
-    process::Command,
-};
-use fs::File;
-use ron::de::from_reader;
-use figlet_rs::FIGfont;
-use termimad::{Area, MadSkin};
-use syntect::easy::HighlightLines;
-use syntect::highlighting::{Style, ThemeSet};
-use syntect::parsing::SyntaxSet;
-use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
 #[derive(Debug, Deserialize)]
 struct Slides {
@@ -40,18 +36,18 @@ enum FileTypes {
     Code(String, String),
 }
 
-fn text_size(s: &str) -> (usize,usize) {
+fn text_size(s: &str) -> (usize, usize) {
     let w = 1 + s.lines().fold(0, |acc, l| acc.max(l.len()));
 
-    (w,s.lines().count())
+    (w, s.lines().count())
 }
 
 impl FileTypes {
-    fn write_text(buf: &mut Surface, txt:&String) -> Result<(), Error> {
-        let (width, height) =  buf.dimensions();
-        let top = height.saturating_sub(txt.lines().count())  /2;
-        
-        for (idx,l) in txt.lines().enumerate() {
+    fn write_text(buf: &mut Surface, txt: &String) -> Result<(), Error> {
+        let (width, height) = buf.dimensions();
+        let top = height.saturating_sub(txt.lines().count()) / 2;
+
+        for (idx, l) in txt.lines().enumerate() {
             let x = width.saturating_sub(l.len()) / 2;
             buf.add_change(Change::CursorPosition {
                 x: Position::Absolute(x),
@@ -68,30 +64,31 @@ impl FileTypes {
             FileTypes::Text(txt) => {
                 buf.add_change(Change::ClearScreen(ColorAttribute::Default));
                 buf.add_change(Change::CursorVisibility(CursorVisibility::Hidden));
-                
-                const DATA: &'static [u8] = include_bytes!("../assets/giphy3.gif");
-                let image_data = Arc::new(ImageData::with_raw_data(DATA.to_vec().into_boxed_slice()));
-                
-                buf.add_change(Change::CursorPosition {
-                    x: Position::Absolute(0),
-                    y: Position::Absolute(0),
-                });
-                buf.add_change(Change::Image(termwiz::surface::change::Image {
-                    width: 5 as usize,
-                    height: 5 as usize,
-                    top_left: TextureCoordinate::new_f32(0.,0.),
-                    bottom_right: TextureCoordinate::new_f32(1., 1.),
-                    image: Arc::clone(&image_data),
-                }));
-                buf.flush_changes_older_than(0);
+                // const DATA: &'static [u8] = include_bytes!("../assets/giphy3.gif");
+                // let image_data = Arc::new(ImageData::with_raw_data(DATA.to_vec().into_boxed_slice()));
 
-                // Self::write_text(buf, txt)?;
+                // buf.resize(180, 40);
+                // buf.add_change(Change::CursorPosition {
+                //     x: Position::Absolute(0),
+                //     y: Position::Absolute(0),
+                // });
+                // buf.flush_changes_older_than(0);
+                // buf.add_change(Change::Image(termwiz::surface::change::Image {
+                //     width: 15 as usize,
+                //     height: 15 as usize,
+                //     top_left: TextureCoordinate::new_f32(0.,0.),
+                //     bottom_right: TextureCoordinate::new_f32(1.,1.),
+                //     image: Arc::clone(&image_data),
+                // }));
+                // buf.flush_changes_older_than(0);
+
+                Self::write_text(buf, txt)?;
             }
             FileTypes::Markdown(path) => {
                 let (width, height) = buf.dimensions();
                 let markdown = fs::read_to_string(Path::new(path))?;
-                let (text_w,_) = text_size(markdown.as_str());
-                let area_w = text_w.min(width - (margin*2)) ;
+                let (text_w, _) = text_size(markdown.as_str());
+                let area_w = text_w.min(width - (margin * 2));
                 let area_h = height / 2;
                 let x = 0.max((width - area_w) / 2) as u16;
                 let y = 0.max((height - area_h) / 2) as u16;
@@ -103,22 +100,22 @@ impl FileTypes {
             FileTypes::Code(path, syntax) => {
                 let (width, height) = buf.dimensions();
                 let content = fs::read_to_string(Path::new(path))?;
-                let text_size = text_size(content.as_str()); 
-                let x = (width - text_size.0 )/2;
-                let y = (height - text_size.1 )/2;
+                let text_size = text_size(content.as_str());
+                let x = (width - text_size.0) / 2;
+                let y = (height - text_size.1) / 2;
                 let ps = SyntaxSet::load_defaults_newlines();
                 let ts = ThemeSet::load_defaults();
 
                 let syntax = ps.find_syntax_by_extension(syntax).unwrap();
                 let mut highlighter = HighlightLines::new(syntax, &ts.themes["Solarized (light)"]);
 
-                for (idx,line) in LinesWithEndings::from(content.as_str()).enumerate() {
+                for (idx, line) in LinesWithEndings::from(content.as_str()).enumerate() {
                     let ranges: Vec<(Style, &str)> = highlighter.highlight(line, &ps);
                     let escaped = as_24_bit_terminal_escaped(&ranges[..], false);
 
                     buf.add_change(Change::CursorPosition {
                         x: Position::Absolute(x),
-                        y: Position::Absolute(y+idx),
+                        y: Position::Absolute(y + idx),
                     });
                     buf.add_change(format!("{}", escaped.to_string()));
                 }
@@ -140,14 +137,15 @@ fn main() -> Result<(), Error> {
         .version("0.1.0")
         .author("@mib")
         .about("terminal presenting")
-        .arg(Arg::with_name("file")
+        .arg(
+            Arg::with_name("file")
                 .short("f")
                 .long("file")
                 .takes_value(true)
                 .required(true)
-                .help("input file (*.ron)"))
+                .help("input file (*.ron)"),
+        )
         .get_matches();
-
 
     let mut idx = 0_usize;
     let margin = 2_usize;
